cdd24be88659d400ca58b1ce85935b6d30f8b57d
lritchis <lritchis@purdue.edu>
2021-09-21 15:32:37 -0400

    Initial

---
+++ lritchis/Address.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include "Address.h"
+
+Address::Address(std::string _street, std::string _city, std::string _state, int _zip) {
+   street = _street;
+   city = _city;
+   state = _state;
+   zip = _zip;
+}
+
+int Address::getZipCode( ) {
+   return zip;
+}
+
+std::string Address::getAddress( ) {
+   return street + "\n" + city + "\n" + state + "\n" + std::to_string(getZipCode());
+}
