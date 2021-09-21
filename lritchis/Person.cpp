c408e55a1777366ba18f6a75f1f81c0bcabfac11
lritchis <lritchis@purdue.edu>
2021-09-21 17:39:34 -0400

    WHy

--- lritchis/Person.cpp
+++ lritchis/Person.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "Person.h"
 
-Person::Person(std::string _name, int _birthMonth, int _birthDay, int _birthYear, Address _address) : address(_address), name(_name) { 
+Person::Person(std::string _name, int _birthMonth, int _birthDay, int _birthYear, Address _address) : name(_name), address(_address)  { 
    dateOfBirth = std::string(std::to_string(_birthMonth)+"/"+std::to_string(_birthDay)+"/"+std::to_string(_birthYear)); // this should be the only line in the constructor body
 }
 
